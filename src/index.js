721cd618156c3b936efaaf3e20b388bb6775fc30
Jason Pau <jason@jasonpau.com>
2017-08-09 14:52:49 -0700

    Configured store and middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+//import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './App';
+
+//const middleware = [thunk];
+// const store = createStore(applyMiddleware(thunk));
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
