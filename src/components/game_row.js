bfa6a5df8826537e869c11eb35fe7e96b027ff6a
Jason Pau <jason@jasonpau.com>
2017-08-09 19:38:48 -0700

    CSS updates for board

--- src/components/game_row.js
+++ src/components/game_row.js
@@ -3,15 +3,11 @@ import React, { Component } from 'react';
 
 //function receives props telling it to make X objects per array
 export default function GameRow(props) {
-    //const num = props;
-  console.log(props);
 
     const cells = props.row.map((cell, index) => {
-
-      console.log(cell, index);
       return (
-        <div key={index} className={cell.color}>
-          cell
+        <div key={index} className={'game-row-cell color-' + cell.color}>
+
         </div>
       )
     });
